29442a42787675c54c0f717bf586eba190fc8a31
Deanna Kelley <deanna.georges@gmail.com>
2019-10-31 17:52:47 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -12,11 +12,13 @@ class Song
     self.class.all << self
   end
   
-  def self.create
-    song = self.new
-    @@all << song
-    song
+  def self.create #class constructor
+    song = self.new # initializes a new song
+    @@all << song # saves it to the class variable 
+    song # returns the instance 
   end 
+  
+  def self.new_by_name(song_name)
   
   
 #binding.pry
