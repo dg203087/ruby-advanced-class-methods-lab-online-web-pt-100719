f7e3cf0b9c9f28f96603ed8388f81d039bf63df5
Deanna Kelley <deanna.georges@gmail.com>
2019-10-31 18:47:37 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -51,8 +51,9 @@ class Song
   
   def self.new_from_filename(file_name)
     song = self.new
-    song.name = (song_name.split(" - ")[1].chomp(".mp3"))
-    song.artist_name = (song_name.split(" - ")[0])
+    song.name = (song_name.split(" - ")[1].chomp(".mp3")) #splits input and sets the second instance in the array (name of song) equal to song.name. Removes the file type
+    song.artist_name = (song_name.split(" - ")[0]) #splits input and sets the first instance in the array (artist name) equal to artist_name 
+    song 
   end 
   
   def self.create_from_filename
