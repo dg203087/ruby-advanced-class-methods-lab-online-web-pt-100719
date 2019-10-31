adb81fe24c2814b70bd0d25a812747824e77a3a7
Deanna Kelley <deanna.georges@gmail.com>
2019-10-31 18:23:43 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -42,7 +42,7 @@ class Song
     #  self.find_by_name(name)
     #end
     #refactor
-    self.find_by_name || self.create_by_name
+    self.find_by_name(song_name) || self.create_by_name(song_name)
   end 
   
 #binding.pry
